252625d8cc17e24ec2aaebff5facbc466fca502d
Attila Tóth <toth_attila@outlook.hu>
2022-01-22 16:33:58 +0100

    Finished challenge.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
+let holder;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+
+  coll[i].addEventListener("click", function() { 
+    for (i = 0; i < coll.length; i++){
+        if(coll[i].classList.length == 2) {
+            holder = coll[i];
+            coll[i].classList.toggle("active");
+            var content = coll[i].nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                } else {
+                content.style.display = "block";
+                }
+            } 
+    }
+    if(holder == this){
+
+    } else {
+
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -10,5 +28,10 @@ for (i = 0; i < coll.length; i++) {
     } else {
       content.style.display = "block";
     }
-  });
-} 
+    
+
+}
+holder = 0; 
+
+})
+};
